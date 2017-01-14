c4bfe5135f2b195a7f0546fd99232d6c7efdf1e2
Fabian Ochsner <f.ochsn3r@gmail.com>
2017-01-14 21:55:32 +0100

    -Start screen

--- Zavtra/Resources/Resource.Designer.cs
+++ Zavtra/Resources/Resource.Designer.cs
@@ -45,7 +45,16 @@ namespace Zavtra
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int ButtonContinueStyle = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int ButtonNewGameStyle = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int ProgressBarStyle = 2130837507;
 			
 			static Drawable()
 			{
@@ -53,6 +62,34 @@ namespace Zavtra
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050003
+			public const int btnContinue = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int btnNewGame = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int progressBar1 = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int txtOr = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int txtZavtra = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
 			{
 			}
 		}
