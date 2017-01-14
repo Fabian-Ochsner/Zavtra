7065c1d397687217982e9d22209bbff81c983e4e
Fabian Ochsner <f.ochsn3r@gmail.com>
2017-01-14 20:05:40 +0100

    Add project files.

---
+++ Zavtra/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Zavtra
+{
+    [Activity(Label = "Zavtra", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
