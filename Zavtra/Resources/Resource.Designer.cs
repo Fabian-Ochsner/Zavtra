86829a235c315d5f8dbc208bf4ba26054446f97b
Fabian Ochsner <f.ochsn3r@gmail.com>
2017-01-16 18:50:49 +0100

    layout

--- Zavtra/Resources/Resource.Designer.cs
+++ Zavtra/Resources/Resource.Designer.cs
@@ -75,32 +75,41 @@ namespace Zavtra
 			// aapt resource value: 0x7f050001
 			public const int btnNewGame = 2131034113;
 			
-			// aapt resource value: 0x7f05000c
-			public const int button1 = 2131034124;
+			// aapt resource value: 0x7f05000a
+			public const int button1 = 2131034122;
 			
 			// aapt resource value: 0x7f050004
 			public const int progressBar1 = 2131034116;
 			
 			// aapt resource value: 0x7f050005
-			public const int relativeLayout1 = 2131034117;
+			public const int tableLayout1 = 2131034117;
 			
 			// aapt resource value: 0x7f050006
-			public const int textView1 = 2131034118;
-			
-			// aapt resource value: 0x7f050007
-			public const int textView2 = 2131034119;
-			
-			// aapt resource value: 0x7f050008
-			public const int textView3 = 2131034120;
-			
-			// aapt resource value: 0x7f050009
-			public const int textView4 = 2131034121;
-			
-			// aapt resource value: 0x7f05000a
-			public const int textView5 = 2131034122;
+			public const int tableRow1 = 2131034118;
 			
 			// aapt resource value: 0x7f05000b
-			public const int textView6 = 2131034123;
+			public const int tableRow3 = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int tableRow4 = 2131034124;
+			
+			// aapt resource value: 0x7f05000d
+			public const int tableRow5 = 2131034125;
+			
+			// aapt resource value: 0x7f05000e
+			public const int tableRow6 = 2131034126;
+			
+			// aapt resource value: 0x7f05000f
+			public const int tableRow7 = 2131034127;
+			
+			// aapt resource value: 0x7f050007
+			public const int textView22 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int textView23 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int textView24 = 2131034121;
 			
 			// aapt resource value: 0x7f050002
 			public const int txtOr = 2131034114;
