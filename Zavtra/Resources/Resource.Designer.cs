a809eee23f7f85cd0005e299bfdb426e60f6ad14
Fabian Ochsner <f.ochsn3r@gmail.com>
2017-01-31 16:37:32 +0100

    dialog fragment

--- Zavtra/Resources/Resource.Designer.cs
+++ Zavtra/Resources/Resource.Designer.cs
@@ -164,10 +164,13 @@ namespace Zavtra
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int DialogBuildingList = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Town = 2130903041;
+			public const int Main = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Town = 2130903042;
 			
 			static Layout()
 			{
--- Zavtra/TownActivity.cs
+++ Zavtra/TownActivity.cs
@@ -20,6 +20,11 @@ namespace Zavtra
         private Button mBtnNewQuarry;
         private Button mBtnNewLumberjack;
         private Button mBtnNewStorehouse;
+        private Button mBtnResidence;
+        private Button mBtnFarm;
+        private Button mBtnQuarry;
+        private Button mBtnLumberjack;
+        private Button mBtnStorehouse;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -47,6 +52,11 @@ namespace Zavtra
             mBtnNewQuarry = FindViewById<Button>(Resource.Id.btnNewQuarry);
             mBtnNewLumberjack = FindViewById<Button>(Resource.Id.btnNewLumberjack);
             mBtnNewStorehouse = FindViewById<Button>(Resource.Id.btnNewStorehouse);
+            mBtnResidence = FindViewById<Button>(Resource.Id.btnResidence);
+            mBtnFarm = FindViewById<Button>(Resource.Id.btnFarm);
+            mBtnQuarry = FindViewById<Button>(Resource.Id.btnQuarry);
+            mBtnLumberjack = FindViewById<Button>(Resource.Id.btnLumberjackHut);
+            mBtnStorehouse = FindViewById<Button>(Resource.Id.btnStorehouse);
 
             mBtnNewResidence.Click += (object sender, EventArgs args) =>
             {
