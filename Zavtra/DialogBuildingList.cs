7565aae527261480e7f8840d1f8f1e7b6fd897ef
Fabian Ochsner <f.ochsn3r@gmail.com>
2017-02-05 16:53:26 +0100

    -error fixt - dialog fragment not working

--- Zavtra/DialogBuildingList.cs
+++ Zavtra/DialogBuildingList.cs
@@ -18,7 +18,6 @@ namespace Zavtra
         BuildingType mType;
         private ListView mListView;
         private List<Structure> mBuildings;
-
         
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -29,12 +28,10 @@ namespace Zavtra
                 mType = (BuildingType)args.GetInt("Type");
             }
             base.OnCreateView(inflater, container, savedInstanceState);
-
-            var view = inflater.Inflate(Resource.Layout.DialogBuildingList, container, false);
             mBuildings = new List<Structure>();
             foreach (var building in TownActivity.zavtra.structures)
             {
-                if(building.building == mType)
+                if (building.building == mType)
                 {
                     mBuildings.Add(building);
                 }
@@ -44,9 +41,11 @@ namespace Zavtra
             //mBuildingList = view.FindViewById<ListView>(Resource.Id.livBuilding);
             //ArrayAdapter<string> adapter = new ArrayAdapter<Structure>(this, Android.Resource.Layout.SimpleListItem1, mBuildings);
 
-            BuildingListAdapter adapter = new BuildingListAdapter(this, mBuildings);
+            BuildingListAdapter adapter = new BuildingListAdapter(this.Activity, mBuildings);
 
 
+            var view = inflater.Inflate(Resource.Layout.DialogBuildingList, container, false);
+            
 
 
             return view;
