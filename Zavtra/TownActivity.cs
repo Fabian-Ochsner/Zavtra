de51077eb40377945085384f93d03c7d267090e8
Fabian Ochsner <f.ochsn3r@gmail.com>
2017-02-27 18:26:49 +0100

    -not used using' removed

--- Zavtra/TownActivity.cs
+++ Zavtra/TownActivity.cs
@@ -1,16 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
-using Newtonsoft.Json;
-using System.Timers;
 using System.Threading.Tasks;
 using System.IO;
 
